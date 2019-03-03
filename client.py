129061fd797104ff7ffad2333ed691f3d027edd0
Shubham Saini <shubham6405@gmail.com>
2019-03-03 23:18:37 +0530

    Finally working with python2

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import sys
   
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 if len(sys.argv) != 3: 
-    print ("Correct usage: script, IP address, port number")
+    print "Correct usage: script, IP address, port number"
     exit() 
 IP_address = str(sys.argv[1]) 
 Port = int(sys.argv[2]) 
@@ -12,14 +12,14 @@ server.connect((IP_address, Port))
   
 while True: 
   
-      sockets_list = [sys.stdin, server] 
+    sockets_list = [sys.stdin, server] 
   
     read_sockets,write_socket, error_socket = select.select(sockets_list,[],[]) 
   
     for socks in read_sockets: 
         if socks == server: 
             message = socks.recv(2048) 
-            print (message)
+            print message 
         else: 
             message = sys.stdin.readline() 
             server.send(message) 
@@ -27,3 +27,4 @@ while True:
             sys.stdout.write(message) 
             sys.stdout.flush() 
 server.close() 
+
